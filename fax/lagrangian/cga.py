b13d7a4b1ef6de39c59d81822cbfbcd587365f2d
gehring <clement.gehring@gmail.com>
2019-08-23 12:49:57 -0400

    Changed the CGA optimizer to follow a jax optimizer like API. CGA now allows warm start of the inner linear op solve.

--- fax/lagrangian/cga.py
+++ fax/lagrangian/cga.py
@@ -1,3 +1,5 @@
+import collections
+
 import jax
 import jax.numpy as np
 from jax.experimental import optimizers
@@ -5,6 +7,8 @@ from jax.experimental import optimizers
 from fax import converge
 from fax import loop
 from fax.loop import FixedPointSolution
+
+CGAState = collections.namedtuple("CGAState", "x y delta_x delta_y")
 
 
 def make_mixed_jvp(f, x, y, reversed=False):
@@ -44,9 +48,11 @@ def cga(step_size_f, step_size_g, f, g, linear_op_solver=None,
             rtol, atol = converge.adjust_tol_for_dtype(1e-6, 1e-6, x_new.dtype)
             return converge.max_diff_test(x_new, x_old, rtol, atol)
 
-        def default_solver(amat_op, b):
+        def default_solver(amat_op, b, init_x=None):
+            if init_x is None:
+                init_x = b
             return loop.fixed_point_iteration(
-                init_x=b,
+                init_x=init_x,
                 func=lambda i, x: amat_op(x) + b,
                 convergence_test=default_convergence_test,
                 max_iter=default_max_iter,
@@ -56,8 +62,22 @@ def cga(step_size_f, step_size_g, f, g, linear_op_solver=None,
     step_size_f = optimizers.make_schedule(step_size_f)
     step_size_g = optimizers.make_schedule(step_size_g)
 
+    def init(inputs):
+        return CGAState(
+            x=inputs[0],
+            y=inputs[1],
+            delta_x=np.zeros_like(inputs[0]),
+            delta_y=np.zeros_like(inputs[1]),
+        )
+
     def update(i, grads, inputs):
-        x, y = inputs
+        if len(inputs) < 4:
+            x, y = inputs
+            delta_x = None
+            delta_y = None
+        else:
+            x, y, delta_x, delta_y = inputs
+
         grad_xf, grad_yg = grads
         eta_f = step_size_f(i)
         eta_g = step_size_g(i)
@@ -66,18 +86,25 @@ def cga(step_size_f, step_size_g, f, g, linear_op_solver=None,
         jvp_yxg = make_mixed_jvp(g, x, y, reversed=True)
 
         bx = grad_xf - eta_f * jvp_xyf(grad_yg)
-        deltax = -linear_op_solver(lambda x: (eta_f ** 2) * jvp_xyf(jvp_yxg(x)),
-                                   bx).value
+        delta_x = -linear_op_solver(
+            amat_op=lambda x: (eta_f ** 2) * jvp_xyf(jvp_yxg(x)),
+            b=bx,
+            init_x=delta_x).value
 
         by = grad_yg - eta_g * jvp_yxg(grad_xf)
-        deltay = -linear_op_solver(lambda x: (eta_g ** 2) * jvp_yxg(jvp_xyf(x)),
-                                   by).value
+        delta_y = -linear_op_solver(
+            amat_op=lambda x: (eta_g ** 2) * jvp_yxg(jvp_xyf(x)),
+            b=by,
+            init_x=delta_y).value
 
-        x = x + eta_f * deltax
-        y = y + eta_g * deltay
-        return x, y
+        x = x + eta_f * delta_x
+        y = y + eta_g * delta_y
+        return CGAState(x, y, delta_x, delta_y)
 
-    return update
+    def get_params(state):
+        return state[:2]
+
+    return init, update, get_params
 
 
 def cga_iteration(init_values, f, g, convergence_test, max_iter, step_size_f,
@@ -110,7 +137,7 @@ def cga_iteration(init_values, f, g, convergence_test, max_iter, step_size_f,
             a callable taking in the current iteration and returning a scalar.
             If no step size is given for `g`, then `step_size_f` is also used
             for `g`.
-        step_size_f (optional): The step size used by CGA for `g`. Like
+        step_size_g (optional): The step size used by CGA for `g`. Like
             `step_size_f`, this can be a scalar or a callable. If no step size
             is given for `g`, then `step_size_f` is used.
         linear_op_solver (callable, optional): This is a function which outputs
@@ -142,7 +169,7 @@ def cga_iteration(init_values, f, g, convergence_test, max_iter, step_size_f,
             size of the last step if desired.
     """
 
-    cga_update = cga(
+    cga_init, cga_update, get_params = cga(
         step_size_f=step_size_f,
         step_size_g=step_size_g or step_size_f,
         f=f,
@@ -154,15 +181,22 @@ def cga_iteration(init_values, f, g, convergence_test, max_iter, step_size_f,
     grad_xf = jax.grad(f, 0)
 
     def step(i, inputs):
-        (x, y) = inputs
+        x, y = inputs[:2]
         grads = (grad_xf(x, y), grad_yg(x, y))
         return cga_update(i, grads, inputs)
 
-    return loop.fixed_point_iteration(
-        init_x=init_values,
+    def cga_convergence_test(x_new, x_old):
+        return convergence_test(x_new[:2], x_old[:2])
+
+    solution = loop.fixed_point_iteration(
+        init_x=cga_init(init_values),
         func=step,
-        convergence_test=convergence_test,
+        convergence_test=cga_convergence_test,
         max_iter=max_iter,
         batched_iter_size=batched_iter_size,
         unroll=unroll,
+    )
+    return solution._replace(
+        value=get_params(solution.value),
+        previous_value=get_params(solution.previous_value),
     )
