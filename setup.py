ed7aaa6577e3532aa5d82d747ad0da5283ef0ab8
gehring <clement.gehring@gmail.com>
2019-08-29 13:52:22 -0700

    Changed find package to include implicit namespace packages

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_namespace_packages
 
 install_requires = ['numpy', 'scipy', 'absl-py', 'jax', 'jaxlib']
 
 setup(
     name='fax',
     version='0.0.1',
-    packages=find_packages(
+    packages=find_namespace_packages(
         exclude=["*.tests", "*.tests.*", "tests.*", "tests"]
     ),
     url='',
