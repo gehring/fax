b4f22efa851cc1af53874fb7dcf0fb0d20d7ab97
gehring <clement.gehring@gmail.com>
2019-09-10 17:19:21 -0400

    Fixed pytree_dot implementation. Added pytree_array_equal.

--- fax/math.py
+++ fax/math.py
@@ -1,8 +1,17 @@
+import operator
+
 from jax import lax
 from jax import numpy as np
 from jax import tree_util
 
 
 def pytree_dot(x, y):
-    partial_dot = tree_util.tree_multimap(np.dot, x, y)
+    partial_dot = tree_util.tree_multimap(
+        lambda arr1, arr2: np.sum(arr1 * arr2), x, y)
     return tree_util.tree_reduce(lax.add, partial_dot)
+
+
+def pytree_array_equal(x, y):
+    is_eq = tree_util.tree_multimap(
+        lambda arr1, arr2: np.array_equal(arr1, arr2), x, y)
+    return tree_util.tree_reduce(operator.and_, is_eq)
