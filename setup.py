c8ae7551961bad8c8a4ac6cc2f4d84ec69682d7f
manuel <manuel.delverme@gmail.com>
2020-03-10 11:20:42 -0500

    added more details to setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,39 @@
-from setuptools import setup, find_namespace_packages
+import setuptools
 
 install_requires = ['numpy', 'scipy', 'absl-py', 'jax', 'jaxlib', 'hypothesis']
 
-setup(
-    name='fax',
+with open("README.md") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='jax-fixedpoint',
     version='0.0.4',
-    packages=find_namespace_packages(
+    description='Implicit and competitive differentiation in JAX.',
+    packages=setuptools.find_namespace_packages(
         include=['*', 'fax.*'],
         exclude=["*.tests", "*.tests.*", "tests.*", "tests"]
     ),
     url='',
-    license='',
+    license='MIT License',
     author='Clement Gehring',
-    author_email='clement.gehring@gmail.com',
-    description='',
+    author_email='fax-dev@gehring.io',
+    long_description=long_description.strip(),
+    long_description_content_type="text/markdown",
     install_requires=install_requires,
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+
+        "License :: OSI Approved :: MIT License",
+
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX",
+
+        "Programming Language :: C++",
+        "Programming Language :: Python :: 3",
+
+        "Topic :: Scientific/Engineering :: Artificial Intelligence"
+    ],
+    python_requires=">=3.5",
 )
