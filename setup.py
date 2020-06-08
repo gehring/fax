5d33f8e9ad48290e57ca61a822968d3181a22550
manuel <manuel.delverme@gmail.com>
2020-06-08 11:47:17 -0400

    reverted setup.py changes and indentation levels

--- setup.py
+++ setup.py
@@ -1,39 +1,18 @@
-import setuptools
+from setuptools import setup, find_namespace_packages
 
 install_requires = ['numpy', 'scipy', 'absl-py', 'jax', 'jaxlib', 'hypothesis']
 
-with open("README.md") as f:
-    long_description = f.read()
-
-setuptools.setup(
-    name='jax-fixedpoint',
+setup(
+    name='fax',
     version='0.0.4',
-    description='Implicit and competitive differentiation in JAX.',
-    packages=setuptools.find_namespace_packages(
+    packages=find_namespace_packages(
         include=['*', 'fax.*'],
         exclude=["*.tests", "*.tests.*", "tests.*", "tests"]
     ),
-    url='https://github.com/gehring/fax',
-    license='MIT License',
+    url='',
+    license='',
     author='Clement Gehring',
-    author_email='fax-dev@gehring.io',
-    long_description=long_description.strip(),
-    long_description_content_type="text/markdown",
+    author_email='clement.gehring@gmail.com',
+    description='',
     install_requires=install_requires,
-    classifiers=[
-        "Intended Audience :: Developers",
-        "Intended Audience :: Science/Research",
-
-        "License :: OSI Approved :: MIT License",
-
-        "Operating System :: MacOS :: MacOS X",
-        "Operating System :: Microsoft :: Windows",
-        "Operating System :: POSIX",
-
-        "Programming Language :: C++",
-        "Programming Language :: Python :: 3",
-
-        "Topic :: Scientific/Engineering :: Artificial Intelligence"
-    ],
-    python_requires=">=3.5",
 )
