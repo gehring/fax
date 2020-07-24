Switched to using jax's implementation of conjugate gradient. Fixed tests to fit new api without an explicit iteration index. Fixed potential for leaky tracers in twophase.py. Updated implicit ecp to new two_phase_solve api. Adjusted tolerances to ensure all tests pass.

fax/constrained/constrained_test.py
@@ -46,27 +46,37 @@ class CGATest(jax.test_util.JaxTestCase):
             return converge.max_diff_test(x_new, x_old, rtol, atol)
 
         @jax.jit
-        def step(i, opt_state):
+        def step(opt_state):
             params = get_params(opt_state)
             grads = jax.grad(lagrangian, (0, 1))(*params)
-            return opt_update(i, grads, opt_state)
+            return opt_update(grads, opt_state)
 
         opt_state = opt_init(lagr_params)
 
         for i in range(500):
             old_params = get_params(opt_state)
-            opt_state = step(i, opt_state)
+            opt_state = step(opt_state)
 
             if convergence_test(get_params(opt_state), old_params):
                 break
 
         final_params = get_params(opt_state)
-        self.assertAllClose(opt_val, func(get_x(final_params)),
-                            check_dtypes=False)
+        self.assertAllClose(
+            opt_val,
+            func(get_x(final_params)),
+            check_dtypes=False,
+            atol=1e-10,
+            rtol=1e-5,
+        )
 
         h = eq_constraints(get_x(final_params))
-        self.assertAllClose(h, tree_util.tree_map(np.zeros_like, h),
-                            check_dtypes=False)
+        self.assertAllClose(
+            tree_util.tree_map(np.zeros_like, h),
+            h,
+            check_dtypes=False,
+            atol=1e-5,
+            rtol=1e-5,
+        )
 
     @parameterized.parameters(
         {'method': cga_ecp, 'kwargs': {'max_iter': 1000, 'lr_func': 0.5}},
@@ -93,8 +103,11 @@ class CGATest(jax.test_util.JaxTestCase):
 
         self.assertAllClose(
             objective(*opt_solution),
-            objective(*solution.value),
-            check_dtypes=False)
+            objective(*solution),
+            check_dtypes=False,
+            atol=1e-10,
+            rtol=1e-5,
+        )
 
     @parameterized.parameters(
         {'method': implicit_ecp,
@@ -138,9 +151,11 @@ class CGATest(jax.test_util.JaxTestCase):
             np.zeros_like(true_reward),
             (np.zeros_like(true_transition), np.zeros_like(true_reward))
         )
-        solution = method(objective, equality_constraints, initial_values, **kwargs)
+        solution = method(
+            objective, equality_constraints, initial_values, **kwargs)
 
-        self.assertAllClose(objective(*solution.value), optimal_value, check_dtypes=False)
+        self.assertAllClose(
+            objective(*solution), optimal_value, check_dtypes=False, rtol=1e-5)
 
 
 if __name__ == "__main__":

fax/implicit/twophase.py
@@ -2,6 +2,7 @@ import functools
 import logging
 
 import jax
+import jax.numpy as np
 
 from fax import converge
 from fax import loop
@@ -32,7 +33,7 @@ def default_solver(param_func, default_rtol=1e-10, default_atol=1e-10,
     return _default_solve
 
 
-@functools.partial(jax.custom_vjp, nondiff_argnums=(0, 1, 3))
+@functools.partial(jax.custom_vjp, nondiff_argnums=(0, 3))
 def two_phase_solve(param_func, init_xs, params, solvers=()):
     if solvers:
         fwd_solver = solvers[0]
@@ -47,8 +48,7 @@ def two_phase_fwd(param_func, init_xs, params, solvers):
     return sol, (sol, params)
 
 
-def two_phase_rev(param_func, init_xs, solvers, res, sol_bar):
-    del init_xs
+def two_phase_rev(param_func, solvers, res, sol_bar):
 
     def param_dfp_fn(packed_params):
         v, p, dvalue = packed_params
@@ -66,7 +66,7 @@ def two_phase_rev(param_func, init_xs, solvers, res, sol_bar):
                            (sol, params, sol_bar),
                            solvers[1:])
     _, dparam_vjp = jax.vjp(lambda p: param_func(p)(sol), params)
-    return dparam_vjp(dsol)
+    return jax.tree_map(np.zeros_like, sol), dparam_vjp(dsol)[0]
 
 
 two_phase_solve.defvjp(two_phase_fwd, two_phase_rev)