24719d6ad9f457e6e91ee3f08f7a5fa141cefc83
gehring <clement.gehring@gmail.com>
2020-07-29 14:37:02 -0400

    Add second order test for two_phase_solve

--- fax/implicit/twophase_test.py
+++ fax/implicit/twophase_test.py
@@ -3,6 +3,10 @@ from absl.testing import absltest
 from fax import test_util
 from fax.implicit import twophase
 
+import numpy as np
+
+import jax
+import jax.numpy as jnp
 from jax.config import config
 config.update("jax_enable_x64", True)
 
@@ -13,6 +17,20 @@ class TwoPhaseOpsTest(test_util.FixedPointTestCase):
         def solve(x, params):
             return twophase.two_phase_solve(param_func, x, params)
         return solve
+
+    def testSecondOrder(self):
+        step_size = 0.1
+
+        def sqrt_step(target):
+            def _sqrt_step(x):
+                return x + step_size * (target - x**2)
+            return _sqrt_step
+
+        solver = self.make_solver(sqrt_step)
+
+        test_input = jnp.array([0.1, 0.5, 2., 10.])
+        out = jax.vmap(jax.grad(jax.grad(lambda x: solver(x, x))))(test_input)
+        np.testing.assert_allclose(out, -0.25 * test_input**(-3/2))
 
 
 if __name__ == "__main__":
