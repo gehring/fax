Fixed CMD update computation to accommendate general step sizes (non-uniform for a single player's individual variables). Augmented the bounded Bregman potential generation with non-uniform step size.

fax/competitive/cmd/cmd.py
@@ -130,15 +130,18 @@ def make_pd_bregman(step_size=1e-5):
     return BregmanPotential(DP_pd, DP_inv_pd, D2P_pd, inv_D2P_pd)
 
 
-def make_bound_breg(lb=jnp.array([-1.0]), ub=jnp.array([1.0])):
+def make_bound_breg(lb=jnp.array([-1.0]), ub=jnp.array([1.0]), step_size = jnp.array([1e-4])):
+
     def breg_bound_internal(vec):
         assert vec.shape == lb.shape, "lower bound shape does not match variable shape!"
         assert vec.shape == ub.shape, "upper bound shape does not match variable shape!"
-        return jnp.sum((- vec + ub) * jnp.log(- vec + ub) + (vec - lb) * jnp.log(vec - lb), axis=-1)
+        assert step_size.shape == vec.shape, "eta_ shape does not match variable shape!"
+        return jnp.sum( 1./step_size * ((- vec + ub) * jnp.log(- vec + ub) + (vec - lb) * jnp.log(vec - lb)), axis=-1)
 
     DP_bound_internal = jax.grad(breg_bound_internal)
 
     def DP_inv_bound_internal(vec):
+        vec = step_size * vec
         return (ub * jnp.exp(vec) + lb) / (1 + jnp.exp(vec))
 
     def D2P_bound_internal(vec):
@@ -146,13 +149,14 @@ def make_bound_breg(lb=jnp.array([-1.0]), ub=jnp.array([1.0])):
 
     def inv_D2P_bound_internal(vec):
         if len(jnp.shape(vec)) >= 1:
-            return lambda u: jnp.dot(jnp.diag(1 / ((1 / (ub - vec)) + (1 / (vec - lb)))), u)
+            return lambda u: jnp.dot(jnp.diag( step_size * 1 / ((1 / (ub - vec)) + (1 / (vec - lb)))), u)
         else:
-            return lambda u: (1 / ((1 / (ub - vec)) + (1 / (vec - lb)))) * u
+            return lambda u: ( step_size * 1 / ((1 / (ub - vec)) + (1 / (vec - lb)))) * u
 
 
     return BregmanPotential(DP_bound_internal, DP_inv_bound_internal,
                             D2P_bound_internal, inv_D2P_bound_internal)
+
 
 
 # usage: hessian_xy((min_P,max_P))(max_P)
@@ -331,9 +335,9 @@ UpdateState = collections.namedtuple("UpdateState", "del_min del_max")
 _tree_apply = partial(jax.tree_multimap, lambda f, x: f(x))
 
 
-def updates(prev_state, eta_min, eta_max, hessian_xy=None, hessian_yx=None, grad_min=None,
-            grad_max=None, breg_min=default_breg, breg_max=default_breg, objective_func=None,
-            precond_b_min=True, precond_b_max=True):
+def updates(prev_state, hessian_xy=None, hessian_yx=None, grad_min=None,
+            grad_max=None, breg_min=default_breg, breg_max=default_breg, eta_min=1., eta_max=1., objective_func=None,
+            precond_b_min=False, precond_b_max=False):
     """Equation (4). Given current position (prev_state), compute the updates (del_x,del_y) to the players in cmd algorithm for next position.
 
     Args:
@@ -403,6 +407,7 @@ def updates(prev_state, eta_min, eta_max, hessian_xy=None, hessian_yx=None, grad
     temp = hessian_xy(_tree_apply(_tree_apply(breg_max.inv_D2P, prev_state.maxPlayer), grad_max))
     temp2 = tree_util.tree_map(lambda x: eta_max * x, temp)
     vec_min = tree_util.tree_multimap(lambda arr1, arr2: arr1 + arr2, grad_min, temp2)
+
     if precond_b_min:
         # vec_min_tree, min_tree_def = tree_util.tree_flatten(vec_min)
         # cond_min = tree_util.tree_unflatten(min_tree_def,
@@ -411,12 +416,11 @@ def updates(prev_state, eta_min, eta_max, hessian_xy=None, hessian_yx=None, grad
         cond_min = max(jax.tree_map(lambda x: jnp.linalg.norm(x, jnp.inf), tree_util.tree_flatten(vec_min)[0]))
         vec_min = tree_util.tree_map(lambda x: x / cond_min, vec_min)
 
-
-
     # temp = _tree_apply(hessian_yx, _tree_apply(_tree_apply(breg_min.inv_D2P, prev_state.minPlayer), grad_min))
     temp = hessian_yx(_tree_apply(_tree_apply(breg_min.inv_D2P, prev_state.minPlayer), grad_min))
     temp2 = tree_util.tree_map(lambda x: eta_min * x, temp)
     vec_max = tree_util.tree_multimap(lambda x, y: x - y, grad_max, temp2)
+
     if precond_b_max:
         # vec_max_tree, max_tree_def = tree_util.tree_flatten(vec_max)
         # cond_max = tree_util.tree_unflatten(max_tree_def,
@@ -426,14 +430,15 @@ def updates(prev_state, eta_min, eta_max, hessian_xy=None, hessian_yx=None, grad
         vec_max = tree_util.tree_map(lambda x: x / cond_max, vec_max)
 
 
-    update_min, status_min = linalg.cg(linear_opt_min, vec_min, maxiter=1000, tol=1e-25)
+    update_min, status_min = linalg.cg(linear_opt_min, vec_min, maxiter=1000)
+
     if precond_b_min:
         update_min = tree_util.tree_map(lambda x: cond_min * x, update_min)
         # update_min = tree_util.tree_multimap(lambda x, y: y * x, cond_min, update_min)
 
-    update_min = tree_util.tree_map(lambda x: -x, update_min)
+    update_min = tree_util.tree_map(lambda x: -x, update_min) # negation here!
 
-    update_max, status_max = linalg.cg(linear_opt_max, vec_max, maxiter=1000, tol=1e-25)
+    update_max, status_max = linalg.cg(linear_opt_max, vec_max, maxiter=1000)
     if precond_b_max:
         update_max = tree_util.tree_map(lambda x: cond_max * x, update_max)
         # update_max = tree_util.tree_multimap(lambda x, y: y * x, cond_max, update_max)

fax/competitive/cmd/cmd_tester.py
@@ -107,20 +107,58 @@ def make_bound_breg_original(lb=-1.0, ub=1.0):
     return BregmanPotential(DP_bound_internal(lb, ub), DP_inv_bound_internal(lb, ub),
                             D2P_bound_internal(lb, ub), inv_D2P_bound_internal(lb, ub))
 
-bregman_potential = make_bound_breg()
-bregman_potential_original = make_bound_breg_original()
+
+
+def make_bound_breg_vector_original_unscaled(lb=jnp.array([-1.0]), ub=jnp.array([1.0])):
+    def breg_bound_internal(vec):
+        assert vec.shape == lb.shape, "lower bound shape does not match variable shape!"
+        assert vec.shape == ub.shape, "upper bound shape does not match variable shape!"
+        return jnp.sum((- vec + ub) * jnp.log(- vec + ub) + (vec - lb) * jnp.log(vec - lb), axis=-1)
+
+    DP_bound_internal = jax.grad(breg_bound_internal)
+
+    def DP_inv_bound_internal(vec):
+        return (ub * jnp.exp(vec) + lb) / (1 + jnp.exp(vec))
+
+    def D2P_bound_internal(vec):
+        return lambda u: jvp(DP_bound_internal, (vec,), (u,))[1]
+
+    def inv_D2P_bound_internal(vec):
+        if len(jnp.shape(vec)) >= 1:
+            return lambda u: jnp.dot(jnp.diag(1 / ((1 / (ub - vec)) + (1 / (vec - lb)))), u)
+        else:
+            return lambda u: (1 / ((1 / (ub - vec)) + (1 / (vec - lb)))) * u
+
+
+    return BregmanPotential(DP_bound_internal, DP_inv_bound_internal,
+                            D2P_bound_internal, inv_D2P_bound_internal)
+
+
 x = random.normal(key1)
-assert bregman_potential.DP(jnp.array([x])) == bregman_potential_original.DP(jnp.array([x])), "DP not matching!"
-assert bregman_potential.DP_inv(jnp.array([x])) == bregman_potential_original.DP_inv(jnp.array([x])), "DP_inv not matching!"
-assert bregman_potential.D2P(jnp.array([x]))(jnp.array([1.2345])) == bregman_potential_original.D2P(jnp.array([x]))(jnp.array([1.2345])), "D2P not matching!"
-assert bregman_potential.inv_D2P(jnp.array([x]))(jnp.array([1.2345])) == bregman_potential_original.inv_D2P(jnp.array([x]))(jnp.array([1.2345])), "D2P not matching!"
+eta = jnp.array([1e-2])
+
+bregman_potential = make_bound_breg(step_size=eta)
+bregman_potential_original = make_bound_breg_vector_original_unscaled() #make_bound_breg_original()
+assert bregman_potential.DP(jnp.array([x])) == 1/eta* bregman_potential_original.DP(jnp.array([x])), "DP not matching!"
+assert bregman_potential.DP_inv(jnp.array([x])) == bregman_potential_original.DP_inv(eta* jnp.array([x])), "DP_inv not matching!"
+assert jnp.isclose(bregman_potential.D2P(jnp.array([x]))(jnp.array([1.2345])) , 1/eta * bregman_potential_original.D2P(jnp.array([x]))(jnp.array([1.2345]))), "D2P not matching!"
+assert jnp.isclose(bregman_potential.inv_D2P(jnp.array([x]))(jnp.array([1.2345])) , eta* bregman_potential_original.inv_D2P(jnp.array([x]))(jnp.array([1.2345]))), "D2P not matching!"
 
 lb = -1.0 * jnp.ones((5,))
 ub = 1.0 * jnp.ones((5,))
-bregman_potential = make_bound_breg(lb,ub)
+# eta = jnp.array([1e-2,2e-3,7e-1,4e-4,3e-2])
+eta = 1.234e-2
+vector_eta = eta * jnp.ones((5,))
+bregman_potential = make_bound_breg(lb,ub, vector_eta)
 bregman_potential_original = make_bound_breg_original(lb,ub)
 
-print(bregman_potential_original.DP(x2))
+assert jnp.isclose(bregman_potential.DP(x2) , 1/eta * bregman_potential_original.DP(x2)).all(), "DP not matching!"
+assert jnp.isclose(bregman_potential.DP_inv(x2) , bregman_potential_original.DP_inv(eta* x2)).all(), "DP_inv not matching!"
+assert jnp.isclose(bregman_potential.D2P(x2)(x2) , 1/eta * bregman_potential_original.D2P(x2)(x2)).all(), "D2P not matching!"
+assert jnp.isclose(bregman_potential.inv_D2P(x2)(x2) , eta* bregman_potential_original.inv_D2P(x2)(x2)).all(), "D2P not matching!"
+
+
+print(1 / eta * bregman_potential_original.DP(x2))
 print(bregman_potential.DP(x2))
 
 # cmd main algorithm test with single variables, scalar example from cmd paper